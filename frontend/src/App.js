6572fe1c1fd2fd7334138bdd7748f2d5c3f1848c
Alexis Chaloulos <ale.halu@gmail.com>
2023-05-27 21:29:17 +0300

    controlled search component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,26 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './components/Header';
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+import Search from "./components/Search";
+
+
 
 const App = () => {
+  const [word, setWord] = useState('');
+
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    console.log(word);
+  }
+
+  //console.log(word);
+
   return (
     <div>
-      <Header title="Images Gallery"/>
+      <Header title="Images Gallery" />
+      <Search world={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
     </div>
   );
-}
+};
 
 export default App;
