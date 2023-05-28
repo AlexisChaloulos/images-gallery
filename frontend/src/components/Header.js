56e17000c8f0881e48e52845774640e65f6c22e8
Alexis Chaloulos <ale.halu@gmail.com>
2023-05-28 20:32:53 +0300

    added text logo

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Navbar, Container } from 'react-bootstrap';
-
+import { ReactComponent as Logo } from '../images/logo.svg';
 const navbartStyle = {
-  backgroundColor: 'lightblue',
+  backgroundColor: '#eeeeee',
 };
 
 const Header = ({ title }) => {
   return (
     <Navbar style={navbartStyle} variant="light">
       <Container>
-        <Navbar.Brand href="/">{title}</Navbar.Brand>
+        <Logo style={{ maxWidth: '10rem', maxHeight: '2rem' }} />
       </Container>
     </Navbar>
   );
